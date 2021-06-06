8a89dcc3384838504764633a072b8719505dbc8a
Hassam Siddiqui <62291215+iamhassam143@users.noreply.github.com>
2021-06-06 14:36:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+export default function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Note/>
+    <Note/>
+    <Note/>
+    <Footer/>
+    </div>
+  );
+}
---
+++ Note.js
@@ -0,0 +1,12 @@
+
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This was an amazing bootcamp taken up by
+        Shaury Sinha We covered everything from scratch
+        including Javascript React.js, HTML.
+      </p>
+    </div>
+  );
+}
